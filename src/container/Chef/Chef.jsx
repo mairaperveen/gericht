22bbac99213d57faca4bac8acc1404dd106305b2
Maira Perveen <maira.perveen25@gmail.com>
2023-09-13 21:32:40 +0200

    fix

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">we don't just serve food,we serve our beliefs on a plate. Our commitment to quality, sustainability, and community shines through in every dish, making 
+          dining.the power of food to bring people together and make a positive impact on the world.</p>
+        </div>
+        <p className="p__opensans"> </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
